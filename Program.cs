c76443835dbd82c2e30ceb93533247b8d551569b
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-09-12 15:28:16 +0200

    Added explainatory comments

--- Program.cs
+++ Program.cs
@@ -7,29 +7,36 @@
             Console.WriteLine("Välkommen till PyramidBuilder2000!\nDu ska nu få ange hur många rader din pyramid ska ha.\nAnge en siffra:");
             int levels = Convert.ToInt32(Console.ReadLine());
             int stars = 0;
-            int[] pyramid = new int [levels];
-            
+            int[] pyramid = new int[levels];
 
-            for (int i = 0; i < pyramid.Length; i++)
+
+            for (int i = 0; i < pyramid.Length; i++)//Array-filling loop
             {
                 pyramid[i] = stars++;
-                pyramid[i] = (2 * stars) - 1;
+                pyramid[i] = (2 * stars) - 1; //Math turns rows given by user into number of stars per row.
             }
 
-            foreach (int i in pyramid)
+            foreach (int i in pyramid) //performs the loop underneath for every value in "pyramid".
             {
-                for (int starRow=0; starRow<i; starRow++)
+                for (int starRow = 0; starRow < i; starRow++) 
+                //Writes out no. of stars to the corresponding number in array. 
+                //As long as starRow is less than the value in pyramid (i) it will write a star.
+                //Example: vaule in pyramid=3. starRow will loop 3 times and write a star each time.
+                    
                 {
-                    Console.Write("*");
+                    Console.Write("*"); //writes the stars on one line instead of making a new one
                     
                 }
-                Console.WriteLine("");
+                Console.WriteLine("");//Needs to be here or else all the stars ends up on one long line.
+                
             }
 
+            
+                
+
+            
 
 
-
-           
 
         }
     }
