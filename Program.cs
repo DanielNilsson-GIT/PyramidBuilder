68fadc9f1e6e086bfdc98da7759dcc9b99165788
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-09-19 09:59:22 +0200

    testing other loops and backwards array

--- Program.cs
+++ Program.cs
@@ -8,28 +8,68 @@
             int levels = Convert.ToInt32(Console.ReadLine());
             int stars = 0;
             int[] pyramid = new int[levels];
+            int[] spaces = new int[levels];
 
-
-            for (int i = 0; i < pyramid.Length; i++)//Array-filling loop
+            for (int i=0; i<pyramid.Length; i++)//first we fill up the pyramid array with the correct number of stars on each level of the pyramid
             {
-                pyramid[i] = stars++;
-                pyramid[i] = (2 * stars) - 1; //Math turns rows given by user into number of stars per row.
+                stars++;
+                pyramid[i] = (2 * stars) - 1;
+
+
+            }
+            for (int i=0;i<pyramid.Length;i++)
+            {
+                
+                spaces[i] = pyramid[levels-1];//Then we fill the next array backwards by starting in the end of "pyramids" and working our way down to slot "0"
+                levels--;
             }
 
-            foreach (int i in pyramid) //performs the loop underneath for every value in "pyramid".
+            for (int i = 0; i<pyramid.Length; i++)
             {
-                for (int starRow = 0; starRow < i; starRow++) 
-                //Writes out no. of stars to the corresponding number in array. 
-                //As long as starRow is less than the value in pyramid (i) it will write a star.
-                //Example: vaule in pyramid=3. starRow will loop 3 times and write a star each time.
-                    
+
+                
+                foreach (int j in spaces )
                 {
-                    Console.Write("*"); //writes the stars on one line instead of making a new one
+
+
+                    Console.Write(".");
+                    foreach (int k in pyramid)
+                    {
+                        Console.Write("*");
+                        break;
+                    }
                     
+
                 }
-                Console.WriteLine("");//Needs to be here or else all the stars ends up on one long line.
                 
             }
+
+           
+
+
+
+            //for (int i = 0; i < pyramid.Length; i++)//Array-filling loop
+            //{
+            //    pyramid[i] = stars++;
+            //    pyramid[i] = (2 * stars) - 1; //Math turns rows given by user into number of stars per row.
+            //}
+
+            //foreach (int i in pyramid) //performs the loop underneath for every value in "pyramid".
+            //{
+            //    for (int starRow = 0; starRow < i; starRow++) 
+            //    //Writes out no. of stars to the corresponding number in array. 
+            //    //As long as starRow is less than the value in pyramid (i) it will write a star.
+            //    //Example: vaule in pyramid=3. starRow will loop 3 times and write a star each time.
+                    
+            //    {
+                    
+            //        Console.Write("*"); //writes the stars on one line instead of making a new one
+                    
+            //    }
+                
+            //    Console.WriteLine("");//Needs to be here or else all the stars ends up on one long line.
+                
+            //}
 
             
                 
